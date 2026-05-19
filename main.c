c1b300d2e781a3cf1088d314f7f0063656074e73
Leon Jonsson <leon_sthlm92@hotmail.com>
2026-05-19 18:05:10 +0200

    klar med kod + lagt till videoprov

--- main.c
+++ main.c
@@ -1,5 +1,5 @@
 #include <stdio.h> //printf and scanf
-#include <ctype.h> // för toupper och räkna medelvärdet
+#include <ctype.h> // för toupper
 typedef struct {    //typedef eftersom att det är enklare att skriva "Elev" istället för "struct Elev"
     char namn[11]; //max 10 tecken + null-terminator
     int provpoäng[13]; //13 provpoäng totalt
@@ -7,7 +7,7 @@ typedef struct {    //typedef eftersom att det är enklare att skriva "Elev" ist
 } Elev;
 
 int main() {
-    Elev elever[5];
+    Elev elever[5]; // skapat array av 5 elever, med index 0 - 4.
 
     for (int i = 0; i < 5; i++) // for loop som maxar antalet elever till 5
     {scanf ("%10s", elever[i].namn); //scannar 10 string tecken max, lägger in det i elevernas namn
@@ -19,27 +19,25 @@ for (int i = 0; i < 5; i++) {
     elever[i].namn[0] = toupper(elever[i].namn[0]);
     int summa = 0;  
 
-    for (int j = 0; j < 13; j++) {
+    for (int j = 0; j < 13; j++) { //räknar ut medelvärdet. börjar med summan = elevernas provpoäng loopat 13 gånger.
         summa += elever[i].provpoäng[j];
     }     elever[i].medelvärde = summa / 13.0; // decimaltal på 13:0 för att få ett decimaltal som resultat
 
-
-    //printf ("%s\n", elever[i].namn);
     
     }    
     
-    int vinnar_index = 0;
+    int vinnar_index = 0; //skriva ut högsta medelvärdet.
     double högsta_medel = -1.0; //negativt för att försäkra att elevernas värde kommer vara högre
 
     for (int i = 0; i < 5; i++) {
         if (elever[i].medelvärde > högsta_medel) 
-        { högsta_medel = elever[i].medelvärde;
+        { högsta_medel = elever[i].medelvärde; //sparas till elevens index.
         vinnar_index = i;} // högsta medelvärdet sparas i värdet i efter att den har gått igenom alla elever
     }
     
         printf("%s\n", elever[vinnar_index].namn); //vinnar index så att print skriver rätt elevs index nummer, ie namnet.
 
-        double grupp_summa = 0.0;
+        double grupp_summa = 0.0; // grupp_summa slår ihop medelvärdena på alla elever. följt av grupp_snittvärde som delar med 5.
         for (int i = 0; i < 5; i++) {
             grupp_summa += elever[i].medelvärde;
         } double grupp_snittvärde = grupp_summa / 5.0;
